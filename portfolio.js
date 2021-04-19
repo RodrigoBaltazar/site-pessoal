c93346214707dd84a3390492c82c6ce1633f63e9
RodrigoBaltazar <rodrigo.baltazar@stormtech.com.br>
2021-04-19 10:41:27 -0300

    jquery: portfolio clicou mostrou apenas ano correspondente

--- portfolio.js
+++ portfolio.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
         var id = $(this).attr("id");
         $("#" + id + "_port").toggle();
     })
-})
+}),
 
-
-
+$(function() {
+        let anos = $(".ano-div[id=2020]").on('click', function() {
+            $('.ano-div').toggle();
+            $(this).show();
+        }
+        )}
+)
