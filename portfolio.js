421a87185088abf180ed71dcea23b22ff6ee5718
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2021-12-22 10:46:59 -0800

    javascript portfolio

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,39 @@
+let ano_2021 = document.getElementById('2021');
+let ano_2020 = document.getElementById('2020');
+let ano_2019 = document.getElementById('2019');
 
-$(document).ready(function(){
+let hide_2021 = document.getElementById("2021_port");
+let hide_2020 = document.getElementById("2020_port");
+let hide_2019 = document.getElementById("2019_port");
+
+ano_2021.onclick = function(){
+    if(hide_2021.hidden != false){
+        hide_2021.hidden = false;
+    }else{
+        hide_2021.hidden = true;
+    }
+}
+
+ano_2020.onclick = function(){
+    if(hide_2020.hidden != false){
+        hide_2020.hidden = false;
+    }else{
+        hide_2020.hidden = true;
+    }
+}
+
+ano_2019.onclick = function(){
+    if(hide_2019.hidden != false){
+        hide_2019.hidden = false;
+    }else{
+        hide_2019.hidden = true;
+    }
+}
+
+
+
+
+/*$(document).ready(function(){
     $('.ano-div').click(function(){
         var id = $(this).attr("id");
         $("#" + id + "_port").toggle();
@@ -13,3 +47,4 @@ $(function() {
         }
         )}
 )
+*/
