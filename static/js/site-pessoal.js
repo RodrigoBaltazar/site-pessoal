c42b87bd3a6fb9f00286de1d9921da28b8559d06
RodrigoBaltazar <rodrigo@MacBook-Pro-de-RodrigoBaltazar.local>
2022-05-22 16:21:08 -0300

    backend flask mail sender

---
+++ static/js/site-pessoal.js
@@ -0,0 +1,38 @@
+
+// Animar todos os itens na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
+
+// Ativar carregamento no botão de enviar formulário para
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", ()=>{
+  btnEnviarLoader.style.display = "block";
+  btnEnviar.style.display = "none";
+})
+
+// Tira a mensagem de sucesso depois de 5 segundos
+
+setTimeout(() => {
+  document.querySelector('#alerta').style.display = 'none';
+}, 5000)
