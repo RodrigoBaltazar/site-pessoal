5bae76d94a3e985a5026fa596a7faa98f0a4cf88
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2023-03-19 13:31:33 -0300

    avancando

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <BaltazarBackworld />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaltazarBackworld from './components/BaltazarBackworld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaltazarBackworld
   }
 }
 </script>
@@ -21,6 +21,48 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+  /* CSS reset */
+body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,form,fieldset,input,textarea,p,blockquote,th,td { 
+    margin:0;
+    padding:0;
+}
+html,body {
+    margin:0;
+    padding:0;
+}
+table {
+    border-collapse:collapse;
+    border-spacing:0;
+}
+fieldset,img { 
+    border:0;
+}
+input{
+    border:1px solid #b0b0b0;
+    padding:3px 5px 4px;
+    color:#979797;
+    width:190px;
+}
+address,caption,cite,code,dfn,th,var {
+    font-style:normal;
+    font-weight:normal;
+}
+ol,ul {
+    list-style:none;
+}
+caption,th {
+    text-align:left;
+}
+h1,h2,h3,h4,h5,h6 {
+    font-size:100%;
+    font-weight:normal;
+}
+q:before,q:after {
+    content:'';
+}
+abbr,acronym { border:0;
+}
+
 </style>
---
+++ app/src/components/BaltazarBackworld.vue
@@ -0,0 +1,53 @@
+<template>
+ <div id="background">
+    <div id="card">
+      <h1>Full Stack Developer</h1>
+      <div class="avatar">
+        <img src="../assets/img_avatar.png" alt="Avatar" class="avatar">   
+      </div>
+    </div>
+ </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'BaltazarBackWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#background {
+    background-color: #222;
+}
+
+#card {
+
+}
+
+.avatar {
+  vertical-align: middle;
+  height: 50px;
+  border-radius: 50%;
+}
+h3 {
+  /* margin: 40px 0 0; */
+}
+ul {
+  /* list-style-type: none;
+  padding: 0; */
+}
+li {
+  /* display: inline-block;
+  margin: 0 10px; */
+}
+a {
+  /* color: #42b983; */
+}
+</style>
