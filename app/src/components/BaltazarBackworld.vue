6cbbf0f372e888698cab9a417ba7bf6a51a0b8e4
RodrigoBaltazar <rodrigobaltazarinf@gmail.com>
2023-03-27 17:48:41 -0300

    modal funcionando pero no mucho

--- app/src/components/BaltazarBackworld.vue
+++ app/src/components/BaltazarBackworld.vue
@@ -22,9 +22,16 @@
         <a href="https://www.instagram.com/rodrigobaltazar00/">
           <i class="fa fa-camera" aria-hidden="true"></i>
         </a>
-        <button id="modal">
+        <button @click="toggleVisibility">
           <i class="fa fa-gear"></i>
         </button>
+        <div ref="modal" class="modal" v-show="hidden">
+          <div ref="modal-content" class="modal-content">
+            <button @click="closeModal" class="close-btn">Fechar</button>
+            <h2>Especificações da Página</h2>
+            <p>Kbis: 000</p>
+          </div>
+        </div>
       </div>
     </div>
  </div>
@@ -33,10 +40,28 @@
 
 
 <script>
+// MODAL
 export default {
+  data() {
+    return {
+      isVisible: true
+    };
+  },
   name: 'BaltazarBackWorld',
-  props: {
-    msg: String
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+    openModal(){
+      const modal = this.$refs.modal;
+      const modalContent = this.$refs.modalContent;
+      modal.style.display = 'block';
+      modalContent.classList.add('show');
+    },
+    closeModal(){
+      this.$refs.modal.style.display = 'none';
+      this.$refs.modalContent.classList.remove('show');
+    }
   }
 }
 </script>
@@ -77,5 +102,49 @@ body {
 a {
   margin: 0 5%;
   color: #222;
+}
+
+/* MODAL */
+
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: none;
+}
+
+.modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  width: 90%;
+  max-width: 30rem;
+  text-align: center;
+}
+
+.close-btn {
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  background-color: transparent;
+  border: none;
+  font-size: 1.2rem;
+  cursor: pointer;
+}
+
+/* MODAL MOBILE */
+@media (min-width: 768px) {
+  .modal-content {
+    width: 70%;
+    max-width: 40rem;
+  }
 }
 </style>
